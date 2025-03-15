58e360e22a8cd530fc1991651256769fa5faae73
Himanshu Ranjan <himanshu.1si16ec028@gmail.com>
2025-03-15 21:52:53 +0530

    Wrote main method in quick sort class

--- src/main/java/com/hr/sorting/QuickSort.java
+++ src/main/java/com/hr/sorting/QuickSort.java
@@ -1,4 +1,23 @@
 package com.hr.sorting;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class QuickSort {
+
+    public static void main(String[] args) {
+        int capacity = 20;
+        int[] arr = new int[capacity];
+        Random random =  new Random();
+        for(int i=0;i<capacity;i++){
+            arr[i] = random.nextInt(100);
+        }
+        System.out.println("Unsorted array : " + Arrays.toString(arr));
+
+        quickSort(arr, 0 , arr.length-1);
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+
+    }
 }
