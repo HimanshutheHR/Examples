7d1eaad2eeaf1b415d6668bcc9c2fc3eb5eeac45
Himanshu Ranjan <himanshu.1si16ec028@gmail.com>
2025-03-15 18:01:26 +0530

    Adding HeapSort implementation in sorting package

--- src/main/java/com/hr/sorting/HeapSort.java
+++ src/main/java/com/hr/sorting/HeapSort.java
@@ -1,5 +1,25 @@
 package com.hr.sorting;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Random;
+
 public class HeapSort {
 
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq  =  new PriorityQueue<>();
+
+        Random random =  new Random();
+        for(int i=0;i<20;i++){
+            pq.add(random.nextInt(100));
+        }
+        System.out.println(pq);
+
+        System.out.println("Heap sort: ");
+        int[] arr = new int[20];
+        for(int i=0;i<20;i++){
+            arr[i] = pq.poll();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
