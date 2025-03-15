509da23fd3d5446cb5a2fbafca3322965fe84afe
Himanshu Ranjan <himanshu.1si16ec028@gmail.com>
2025-03-15 17:32:52 +0530

    added java util PriorityQueue Example

---
+++ src/main/java/com/hr/PriorityQueueExamples.java
@@ -0,0 +1,24 @@
+package com.hr;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+public class PriorityQueueExamples {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq  =  new PriorityQueue<>();
+
+        Random random =  new Random();
+        for(int i=0;i<20;i++){
+            pq.add(random.nextInt(100));
+        }
+        System.out.println(pq);
+
+        System.out.println("Heap sort: ");
+        int[] arr = new int[20];
+        for(int i=0;i<20;i++){
+            arr[i] = pq.poll();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
